f500f072fac5b8906405083b24ae552923b8fdaa
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2025-12-11 08:20:47 +0545

    cloned

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(int argc, char	*argv[]){
 	}
 	
 	size_t rows = 0;
-	size_t cols =10;
+	size_t cols =8;
 
 	char *file_to_dump = argv[1]; // TODO: check if file exists
 	
@@ -27,17 +27,24 @@ int main(int argc, char	*argv[]){
 	u16 buffer;
 	size_t row_counter = 0;
 	size_t col_counter = 0;
+	size_t addrs_counter=  0;
+	
+	printf("%06x  ",addrs_counter);
 
 	while (fread(&buffer, sizeof(buffer),1,fp) > 0){
 		
+		printf("%04x  ",buffer);
+
 		col_counter ++;
 		if (col_counter >= cols){
 			printf("\n");
 			col_counter = 0;
 			row_counter ++;
+			
+			addrs_counter = addrs_counter + cols*2; //TODO: 2 bytes in single word
+			printf("%06x  ",addrs_counter);
 		}
 
-		printf("%04x\t",buffer);
 
 		if (row_counter> rows && rows != 0){
 			break;		
